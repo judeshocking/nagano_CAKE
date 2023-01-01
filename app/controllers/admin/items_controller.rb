09bfae69bc2292706ca09e5667178068b993d437
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-01 10:18:01 +0000

    add admin/items/new/作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,11 +3,22 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path
+    else
+      flash[:notice] = "商品情報が正しく保存されていません"
+      redirect_to new_admin_item_path
+    end
   end
 end
