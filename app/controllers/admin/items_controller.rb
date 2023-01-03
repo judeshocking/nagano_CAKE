cf2d41f0a1da352570a8cb030060a678d0504d79
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-03 05:12:37 +0000

    update admin/items/new 修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,15 +10,32 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Itme.find(params[:id])
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_item_path
+      redirect_to admin_items_path
     else
-      flash[:notice] = "商品情報が正しく保存されていません"
+      flash[:item_created_error] = "商品情報が正しく保存されていません"
       redirect_to new_admin_item_path
     end
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_prams)
+      redirect_to admin_item_path(@item)
+    else
+      flash[:item_created_error] = "商品情報が正しくに保存されていません"
+      redirect_to edit_admin_item_path(@item)
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:introduction,:genre_id,:price,:is_active)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  validates :name, presence: true
+	validates :introduction, presence: true
+	validates :price, 		  presence: true
+	validates :is_active, presence: true
+	has_many :cart_items, dependent: :destroy
+	belongs_to :genre
+	enum sale_status: [:販売可, :販売不可]
 end
