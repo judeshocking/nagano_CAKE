2c19310df7424ead39b86dbcd3fbd0736ef7b769
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-07 12:16:28 +0000

    update/admin/customers/show 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_deleted,:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:encrypted_password,:postal_code,:address,:telephone_number])
+  end
+
 end
