a7b3e68f7ebcd8ea50f7c30823b83fe3dac42328
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-07 05:24:31 +0000

    add-admin/customer/show-作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,8 +6,27 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def show
+    @customer = Customer.find(params[:id])
+  end
+
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path
+    else
+     flash[:notice] = "会員情報が正しく保存されませんでした"
+     redirect_to edit_admin_customer_path(@customer)
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:postal_code,:address,:telephone_number,:is_deleted)
   end
 end
