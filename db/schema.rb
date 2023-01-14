3e031134d11aa10b176ca1900c9b1334b8663bcd
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-14 13:27:01 +0000

    update public/home/top

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_02_043825) do
+ActiveRecord::Schema.define(version: 2023_01_14_090623) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2023_01_02_043825) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_id"
     t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
