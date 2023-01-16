7ff3199c9063286ec4f7a56dd1998abdb91a5c78
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-16 13:35:18 +0000

    update public/addresses/edit-index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,7 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @customer = current_customer
-    @addresses = @customer.address
+    @addresses = current_customer.addresses
   end
 
   def create
@@ -10,18 +9,29 @@ class Public::AddressesController < ApplicationController
     @address.customer_id = current_customer.id
     if @address.save
       flash[:notice] = "新しい配送先の登録が完了しました"
-       redirect_to addresses_path
+      redirect_to addresses_path
     else
       @customer = current_customer
-      @addresses = @customer.address
+      @addresses = current_customer.addresses
       flash[:notice] = "新しい配送先の内容に不備があります"
-      render:index
+      render :index
     end
   end
 
 
   def edit
     @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:success] = "配送先の変更内容を保存しました。"
+      redirect_to addresses_path
+    else
+      flash[:success] = "配送先の変更内容に不備があります。"
+      render :edit
+    end
   end
 
   private
