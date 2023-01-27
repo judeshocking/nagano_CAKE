4e34553184f52dd760b3985e25b68a0f688f0130
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-01-27 09:13:44 +0000

    update admin/homes/top 修正

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum production_status: {着手不可:0, 製作待ち:1, 製作中:2, 製作完了:3}
+  enum making_status: {production_not_allowed:0, waiting_for_production:1, production:2, production_completed:3}
 
 end
