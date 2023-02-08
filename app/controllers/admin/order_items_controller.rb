7ad714c8eedc25289a8381915e1523535651230e
EC2 Default User <ec2-user@ip-172-31-0-131.ap-northeast-1.compute.internal>
2023-02-08 09:12:00 +0000

    追加修正2

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,20 +3,18 @@ class Admin::OrderItemsController < ApplicationController
   def update
 
     @order = Order.find_by(params[:id])
-    @order_item = OrderItem.find_by(params[:order_item][:order_item_id])
-    if @order_item.update(order_item_params)
+    @order_item = OrderItem.find(params[:id])
+    if @order_item.update!(order_item_params)
       if @order.order_items.pluck(:making_status).include?("production")
-         @order.status = 2
-         @order.save
+        @order.status = 2
+        @order.save
       else
-         @order.order_items.pluck(:making_status).all?{|status|status == "production_completed"}
-         @order.status = 3
-         @order.save
+        @order.order_items.pluck(:making_status).all?{|status|status == "production_completed"}
+        @order.status = 3
+        @order.save
       end
       flash[:success] = "制作ステータスを変更しました。"
-      redirect_to admin_order_path
-    else
-      redirect_to admin_order_path
+      redirect_to admin_order_path(@order.id)
     end
   end
 
